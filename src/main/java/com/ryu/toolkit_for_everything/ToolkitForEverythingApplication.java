a75a7484d218a4cbaf9690cd1b8c9a2a51cecf2c
Ryu <hungg562002@gmail.com>
2024-10-21 21:49:20 +0900

    init

---
+++ src/main/java/com/ryu/toolkit_for_everything/ToolkitForEverythingApplication.java
@@ -0,0 +1,13 @@
+package com.ryu.toolkit_for_everything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolkitForEverythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolkitForEverythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryu/toolkit_for_everything/ToolkitForEverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryu.toolkit_for_everything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolkitForEverythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
